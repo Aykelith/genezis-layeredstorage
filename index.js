f2a4e919b125cc81b18ae9ff52fa95f647b60c0d
Alexandru Prisacariu <zalexxanderx@gmail.com>
2019-12-05 12:36:59 +0200

    Released version 0.1.1100

--- index.js
+++ index.js
@@ -21,29 +21,66 @@ export default class LayeredStorage {
         this.settings = data.settings || {};
 
         this.get = this.get.bind(this);
+        this.populateLayers = this.populateLayers.bind(this);
     }
 
-    get(key) {
+    async get(key) {
         if (key == null) throw new Error();
         
-        return new Promise(async (resolve, reject) => {
-            let i=0;
-            for (let length=this.layers.length; i < length; ++i) {
-                const v = await this.layers[i](...arguments);
-                if (v !== undefined) resolve(v);
-            }
+        let i=0;
+        for (let length=this.layers.length; i < length; ++i) {
+            const v = await this.layers[i](...arguments);
+            if (v !== undefined) return v;
+        }
 
-            if (this.settings.returnNullIfNotFound) {
-                resolve(null);
-            } else {
-                reject("Not found the value in any layer");
-            }
+        if (this.settings.returnNullIfNotFound) {
+            Promise.resolve(null);
+        } else {
+            Promise.reject("Not found the value in any layer");
+        }
+    }
 
-            if (i != 0 && this.settings.populateIfNotFound) {
-                for (let j=0; j < i; ++j) {
-                    this.settings.populateMethods[j](...arguments);
-                }
+    async populateLayers() {
+        if (i != 0 && this.settings.populateIfNotFound) {
+            for (let j=0; j < i; ++j) {
+                this.settings.populateMethods[j](...arguments);
             }
-        });
+        }
+    }
+}
+
+//= V2 =================================================================================================================
+
+export class UseFailSafeLayerError extends Error {
+    constructor() {
+        super("");
+
+        this.name = this.constructor.name;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+const ConstructorGenezisCheckerConfig2 = deleteOnProduction({
+    main_layer: GenezisChecker.function().required(),
+    fail_safe_layer: GenezisChecker.function().required()
+});
+
+export class LayeredStorage2 {
+    constructor(data) {
+        GenezisChecker(data, ConstructorGenezisCheckerConfig2);
+
+        this.data = data;
+
+        this.get = this.get.bind(this);
+    }
+
+    async get() {
+        try {
+            return await this.data.main_layer(...arguments);
+        } catch (error) {
+            if (error instanceof UseFailSafeLayerError) return await this.data.fail_safe_layer(...arguments);
+            throw error;
+        }
     }
 }
